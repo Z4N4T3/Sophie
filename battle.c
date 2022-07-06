58807dbd6e06cf82919aff345bdd89142fd0e0b6
Me_Jurgen <jurgenq75@gmail.com>
2022-07-05 18:33:49 -0600

    Validacion de coordenadas

--- battle.c
+++ battle.c
@@ -21,9 +21,27 @@
 #include <stdlib.h>
 #define ROW 8 // filas     
 #define COL 8 // columnas 
-#define SEA ''
+#define SEA '#'
+#define BOAT ' '
 #define Nboats 8
 
+    // ROW eq i and x
+    // COL eq j and y
+void paint(char Board[ROW][COL]){
+
+  int j , i;
+  for (i=ROW-1; i >=0; i-- ){
+    printf("%d ", i);
+    for (j=0; j < COL; j++){
+      printf("%c ",Board[i][j]);
+    }
+    printf("\n");
+  }
+  printf("  ");
+  for(j=0; j < COL; j++)
+    printf("%d ",j);
+
+}
 
 
 int main (){
@@ -32,36 +50,37 @@ int main (){
     char Board[8][8];
          // [x][y]
     int boat[8][2];
+    int ships=0;
     int x , y;
     int win =0;
   
     for (i =0; i < ROW; i++){
       for (j=0; j <COL; j++)
-        Board[i][j] = '#';
+        Board[i][j] = SEA;
     }
 
-    // ROW eq i and x
-    // COL eq j and y
+
     do
     {
+        x = y = -1;
+
+
+      do
+      {
         system("cls || clear");
-        for (i=ROW-1; i >=0; i-- ){
-            printf("%d ", i);
-            for (j=0; j < COL; j++){
-                printf("%c ",Board[i][j]);
-            }
-            printf("\n");
-        }
-        printf("  ");
-        for (j=0; j < COL; j++)
-                printf("%d ",j);
-          
-        printf("\n\nCoordenadas del barco en (X): "); scanf("%d",&x);
-        printf("Coordenadas del barco en (Y): "); scanf("%d",&y);
-
-        Board[x][y] =Board[x][y+1]  = ' '; 
-
-    } while (!win);
+        paint(Board);
+        if (Board[y][x] == BOAT)
+          printf("\nLas coordenadas ya estan ocupadas!!!");
+        else if (y >= 8 || x >=8)
+          printf("\nCoordenadas Fuera de rango!!!");
+        printf("\n\nCoordenadas del barco#%d (x,y):\n",ships+1); scanf("%d%d",&x,&y);
+        
+      } while (Board[y][x] == BOAT || (y >= 8 || x >=8));
+      
+        
+        Board[y][x] =Board[y][x+1]  = BOAT; 
+        ships ++;
+    } while (ships <4);
     
 
 
