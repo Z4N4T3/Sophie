5be225255aaf82b84973cd3d19c5ca70270203a5
Me_Jurgen <jurgenq75@gmail.com>
2022-07-05 21:40:40 -0600

    ingame & other f added

--- battle.c
+++ battle.c
@@ -2,27 +2,14 @@
   ⬛ barco
   ⬜ mar
 */
-/*
-      0   1   2   3   4 
-    + - - - - - - - - + 
-  0 | ■ ■ ■ ■ ■ ■ ■ ■ |
-  1 |                 |
-  2 |                 |
-  3 |                 | 
-  4 |                 | 
-  5 |                 | 
-  6 |                 | 
-  7 |                 | 
-    +   -   -   -   - +
 
-
-*/
 #include <stdio.h>
 #include <stdlib.h>
 #define ROW 8 // filas     
 #define COL 8 // columnas 
 #define SEA '#'
 #define BOAT ' '
+#define SINK 'X'
 #define Nboats 8
 
     // ROW eq i and x
@@ -44,27 +31,34 @@ void paint(char Board[ROW][COL]){
 }
 
 
+void Board_reset(char Board[ROW][COL]){
+  int j , i;
+  for (i =0; i < ROW; i++){
+    for (j=0; j <COL; j++)
+      Board[i][j] = SEA;
+  }
+}
+
 int main (){
 
     int i,j, k;
-    char Board[8][8];
+    char Board[ROW][COL];
+    char inGame_Board[ROW][COL];
          // [x][y]
     int boat[8][2];
     int ships=0;
     int x , y;
     int win =0;
   
-    for (i =0; i < ROW; i++){
-      for (j=0; j <COL; j++)
-        Board[i][j] = SEA;
-    }
+    
+    Board_reset(Board);
 
-
+    // colocacion de los barcos
     do
     {
         x = y = -1;
 
-
+      // valicacion de coordenadas
       do
       {
         system("cls || clear");
@@ -83,6 +77,30 @@ int main (){
     } while (ships <4);
     
 
+
+  // empieza el juego
+    Board_reset(inGame_Board);
+
+    int turn =4;
+    do
+    {
+
+      paint(inGame_Board);
+
+      printf("\nTurnos restantes #%d\n", turn);
+      printf("Coordenadas del proyectil (x,y)\n");scanf("%d%d",&x,&y);
+      
+      if (Board[y][x] == BOAT){
+        inGame_Board[y][x] =inGame_Board[y][x+1] = SINK; 
+        printf("Has destruido un barco!!!\n");
+        turn ++;
+      }else{
+        printf("No le diste a nada!!!\n");
+        turn --;  
+      }
+            
+    } while (turn>=0);
+    
 
 
     return 0;
